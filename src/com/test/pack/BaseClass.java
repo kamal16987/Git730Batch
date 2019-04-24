e35409c77705a754a52785d080d89c1d5f473561
Kamalakannan <kamalakkannan_rajamanickam@trimble.com>
2019-04-24 08:36:28 +0530

    Created a baseclass

---
+++ src/com/test/pack/BaseClass.java
@@ -0,0 +1,18 @@
+package com.test.pack;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		System.out.println("Adding my base class");
+
+	}
+	
+	public void launchBrowser() {
+		System.out.println("Method to launch brownser");
+	}
+	
+	public void sendkeys() {
+		System.out.println("Method to type value in textbox");
+	}
+
+}
