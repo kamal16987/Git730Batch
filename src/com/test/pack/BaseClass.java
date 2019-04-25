6b1cf2255ed7294fc8fd28b57829fff614a79617
panneerselvamyourteam <panneerselvamjune7@gmail.com>
2019-04-25 20:21:03 +0530

    MODIFIED THE PROGRAM

--- src/com/test/pack/BaseClass.java
+++ src/com/test/pack/BaseClass.java
@@ -1,18 +1,42 @@
 package com.test.pack;
 
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseClass {
 
-	public static void main(String[] args) {
-		System.out.println("Adding my base class");
-
+	public void displayName() {
+		System.out.println("ENTER YOUR NAME");
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
 	}
-	
+
 	public void launchBrowser() {
-		System.out.println("Method to launch brownser");
-	}
-	
-	public void sendkeys() {
-		System.out.println("Method to type value in textbox");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\HP\\Desktop\\kamal16987\\Git730Batch\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		WebElement UserName = driver.findElement(By
+				.xpath("//a[@class='gb_5d gb_1 gb_kb']"));
+		UserName.click();
+		WebElement txt = driver.findElement(By.xpath("//input[@type='email']"));
+		txt.sendKeys("PANNEERSELVAM");
+		
 	}
 
-}
+	public static void main(String[] args) throws InterruptedException {
+		BaseClass bc = new BaseClass();
+		bc.displayName();
+
+		bc.launchBrowser();
+	}
+
+	
+	}
+
+
