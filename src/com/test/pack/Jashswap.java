af7ac24e4a23aa2a2f0127f760605e1558c23bb3
jaswanth0594 <jaswanthannamreddy@gmail.com>
2019-04-26 09:43:22 +0530

    new  file

---
+++ src/com/test/pack/Jashswap.java
@@ -0,0 +1,22 @@
+package com.test.pack;
+
+import java.util.Scanner;
+
+public class Jashswap {
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the numbers:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("Swapping numbers are:");
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+	}
+
+}
