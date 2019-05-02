58b28b1eeeee227b6e48f8866c651e0d4d9d512e
Gomathykamalakannan <kgomathykamalakannan>
2019-05-02 23:08:06 +0530

    added

--- src/com/test/pack/BaseClass.java
+++ src/com/test/pack/BaseClass.java
@@ -52,7 +52,25 @@ if (originalpanneer.equals(reversepanneer))
    System.out.println("Entered string/number is a palindrome.");  
 else  
    System.out.println("Entered string/number isn't a palindrome.");   
-} } 
+} 
+	// Gomathy
+{  
+	  int r,sum=0,temp;    
+	  int n=454;//It is the number variable to be checked for palindrome  
+	  
+	  temp=n;    
+	  while(n>0){    
+	   r=n%10;  //getting remainder  
+	   sum=(sum*10)+r;    
+	   n=n/10;    
+	  }    
+	  if(temp==sum)    
+	   System.out.println("palindrome number ");    
+	  else    
+	   System.out.println("not palindrome");    
+	}  
+	}  
+
 	
 
 
