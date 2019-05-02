02a7c817eb58fc2c0125a1df0c295b11c85cd42e
jaswanth0594 <jaswanthannamreddy@gmail.com>
2019-05-02 14:26:06 +0530

    add

--- src/com/test/pack/BaseClass.java
+++ src/com/test/pack/BaseClass.java
@@ -2,10 +2,7 @@ package com.test.pack;
 
 public class BaseClass {
 
-	public static void main(String[] args) {
-		System.out.println("Adding my base class");
-
-	}
+	
 	
 	public void launchBrowser() {
 		System.out.println("Method to launch brownser");
@@ -13,6 +10,20 @@ public class BaseClass {
 	
 	public void sendkeys() {
 		System.out.println("Method to type value in textbox");
+		
+	}
+	public static void main(String[] args) {
+		String s="madam", reverse3="";
+		int len = s.length();
+		for (int i = len-1; i >= 0; i--) {
+			reverse3=reverse3+s.charAt(i);
+			}
+		if (s.equals(reverse3)) {System.out.println("palidrome");
+			
+		}
+		else {System.out.println("not palidrome");
+			
+		}
 	}
 
 }
--- src/com/test/pack/alphanumjash.java
+++ src/com/test/pack/alphanumjash.java
@@ -23,9 +23,7 @@ public class alphanumjash{
  
      
       }
- 
-  }
-
+ }
  
 
  
