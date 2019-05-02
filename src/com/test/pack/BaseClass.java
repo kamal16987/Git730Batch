7cca65619f131e9cf2fb55ba8d82fc5c78e74cf4
Gomathykamalakannan <kgomathykamalakannan>
2019-05-02 21:46:38 +0530

    Add palindrome program

--- src/com/test/pack/BaseClass.java
+++ src/com/test/pack/BaseClass.java
@@ -1,5 +1,7 @@
 package com.test.pack;
 
+import java.util.Scanner;
+
 public class BaseClass {
 
 	public static void main(String[] args) {
@@ -15,4 +17,21 @@ public class BaseClass {
 		System.out.println("Method to type value in textbox");
 	}
 
-}
+
+//palindrome program by Gomathy
+
+public static void main1(String args[])  
+{  
+   String original, reverse = "";  
+   Scanner in = new Scanner(System.in);   
+   System.out.println("Enter a string/number to check if it is a palindrome");  
+   original = in.nextLine();   
+   int length = original.length();   
+   for ( int i = length - 1; i >= 0; i-- )  
+      reverse = reverse + original.charAt(i);  
+   if (original.equals(reverse))  
+      System.out.println("Entered string/number is a palindrome.");  
+   else  
+      System.out.println("Entered string/number isn't a palindrome.");   
+}  
+}  
