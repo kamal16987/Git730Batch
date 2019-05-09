1bfe585811aaad00b2e41dbc7987e29f823d6c4e
panneerselvamyourteam <panneerselvamjune7@gmail.com>
2019-05-09 15:00:58 +0530

    added simple constructor program

---
+++ src/com/test/pack/Constructor.java
@@ -0,0 +1,45 @@
+package com.test.pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Constructor {
+
+
+
+	static WebDriver driver;
+	public Constructor(){
+		this(10,20);
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Desktop\\git 730ambatch\\driver\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+	public Constructor(int a,int b){
+		
+		int c=a+b;
+		
+		System.out.println("The sum of A and B is"+c);
+		
+		
+	}
+	public Constructor(WebDriver driver1){
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		
+	}
+	
+
+	public static void main(String[] args) {
+		new Constructor ();
+		new Constructor(driver);
+		
+		
+		
+	
+	}
+
+}
+
+
