63f96abfdccdb867b5659bbb055d30b23c481a0b
panneerselvamyourteam <panneerselvamjune7@gmail.com>
2019-05-13 17:43:28 +0530

    added changes to base class

--- src/com/test/pack/BaseClass.java
+++ src/com/test/pack/BaseClass.java
@@ -1,31 +1,54 @@
 package com.test.pack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class BaseClass {
-   public void launchBrowser() {
-		System.out.println("Method to launch brownser");
-	}	
-	public void sendkeys() {
-		System.out.println("Method to type value in textbox");		
+   public BaseClass() {
+	   int a;
+		int b;
+		int c;			
+		b=3;
+		c=8;
+		a=b+c;
+		System.out.println(a);
+		c=a-b;
+		b=c+b;
+		System.out.println(b);
+		List <String> l4=new ArrayList<String>();
+		int size = l4.size();
+		System.out.println(size);
+	
+			if(l4.isEmpty()){
+				List<String> l7=new ArrayList<String>();
+				l7.add("panneer");
+				l7.add("selam");
+				l7.add("PanneerSelvam");
+				System.out.println(l7);
+			l7.set(1,"Selvam");
+			System.out.println(l7);
+				if(l7.contains("Sel")){
+					List<Integer> l8=new ArrayList<Integer>();
+					l8.add(0);
+					System.out.println(l8);}
+	List<String> newList=new ArrayList<String>();
+    for(int i=0;i<=6;i++){
+    @SuppressWarnings("resource")
+	Scanner sc2=new Scanner(System.in);			
+    newList.add(sc2.nextLine());
+	System.out.println(newList);
+	}}}	
+	public void sendkeys(){
+    System.out.println("Method to type value in textbox");		
 	}	
 	public void mouseoveractions() {
     System.out.println("method to move element");
 	}	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-	String original,reverse="";
-	Scanner sc=new Scanner(System.in);	
-    System.out.println("Enter the string to reverse");
-	original=sc.nextLine();
-	int length=original.length();
-	for(int i=length-1;i>=0;i--)
-		reverse=reverse+original.charAt(i);
-	if(reverse.equals(original)) {
-		System.out.println("Given string is palindrome");
-	}
-	else {
-		System.out.println("Given string is not a palindrome");
-	}
+		@SuppressWarnings("unused")
+		BaseClass b=new BaseClass();
+	
 	// palindrome program by bhanuprasadh
 	String s="madam",reverse1="";
 	System.out.println(s);
@@ -33,61 +56,40 @@ public class BaseClass {
 	int len = s.length();
 	for (int i = len - 1; i >=0 ; i--)
 	{
-		reverse1=reverse1+s.charAt(i);}
-if (s.equals(reverse1)) {System.out.println("palidrome");	
-}		
-else {System.out.println("not palidrome");	
-}
+		reverse1=reverse1+s.charAt(i);
+   if (s.equals(reverse1)) {System.out.println("palidrome");	
+   }}
 // palindrome program panneerselvamyourteam
-String originalpanneer, reversepanneer =""; 
-System.out.println("***panneer***");
-System.out.println("Enter a string/number to check if it is a palindrome"); 
-Scanner in = new Scanner(System.in);   
-originalpanneer = in.nextLine();   
-int lengthpanneer = originalpanneer.length();
-System.out.println( "The length of the entered string is"+lengthpanneer);
-for ( int i = lengthpanneer - 1; i>=0; i-- ) 
-   reversepanneer = reversepanneer + originalpanneer.charAt(i);
-
-if (originalpanneer.equals(reversepanneer))  
-   System.out.println("Entered string/number is a palindrome.");  
-else  
+   String originalpanneer, reversepanneer =""; 
+   System.out.println("***panneer***");
+   System.out.println("Enter a string/number to check if it is a palindrome"); 
+   Scanner in = new Scanner(System.in);   
+   originalpanneer = in.nextLine();   
+   int lengthpanneer = originalpanneer.length();
+   System.out.println( "The length of the entered string is"+lengthpanneer);
+   for ( int i = lengthpanneer - 1; i>=0; i-- ) {
+   reversepanneer = reversepanneer + originalpanneer.charAt(i);}
+   if (originalpanneer.equals(reversepanneer))  {
+   System.out.println("Entered string/number is a palindrome.");  }
+   else  
    System.out.println("Entered string/number isn't a palindrome.");   
-} 
-	// Gomathy
-{  
-	  int r,sum=0,temp;    
-	  int n=454; 
-	  
-	  temp=n;    
-	  while(n>0){    
-	   r=n%10;  
-	   sum=(sum*10)+r;    
-	   n=n/10;    
-	  }    
-	  if(temp==sum)    
-	   System.out.println("palindrome number ");    
-	  else    
-	   System.out.println("not palindrome");    
-	}  
-	 
-	//Gomss
-	 static int r, sum=0, temp;
-	 
-	 {
-			@SuppressWarnings("resource")
-			Scanner sc=new Scanner(System.in);
-			{
-			int num=sc.nextInt();
-			temp=num;
-			while(num>0) {
-				r=num%10;
-				sum=(sum*10)+r;
-				num=num/10;}
-				
-				if(temp==sum) {
-					System.out.println("Palidrome");
-				}
-				else {
-					System.out.println("not a palidrome");
-				}}}}
+	}
+                       //Gomss
+	{
+	int r, sum=0, temp; 
+	@SuppressWarnings("resource")
+	Scanner sc1=new Scanner(System.in);
+	int num=sc1.nextInt();
+	temp=num;
+	while(num>0) {
+	r=num%10;
+	sum=(sum*10)+r;
+	num=num/10;}				
+	if(temp==sum) {
+	System.out.println("Palidrome");}
+	else {
+	System.out.println("not a palidrome");}}}
+			
+			
+			
+
